bc7de8cfac7f8309b2c65b11da5e7f68bc44cf80
Mafe Garcia <garciacarvajalmariafernanda@gmail.com>
2025-07-30 08:55:37 -0500

    .

--- pogramacion_ob/intro03.py
+++ pogramacion_ob/intro03.py
@@ -23,25 +23,25 @@ class Producto :
     def __str__(self):
         return f"{self.nombre} - ${self.precio}(stock: {self.stock}) -fecha creacion : {self.fecha_creacion}"
 print("-----PRODUCTOS------")
-producto1 = Producto(id_producto=1,nombre="Laptop",precio=150.00,stock=10)
-print(producto1)
-producto2 = Producto(id_producto=2,nombre="Monitor LG",precio=200.00,stock=20)
-print(producto2)
-print("--------------")
-# disminuir stock
-exito= producto1.reducir_stock(3)
-print("reduccion exitosa?" , exito)
-print(producto1)
-print("-------------------")
-exito2= producto1.aumento_stock(3)
-print("reduccion exitosa?" , exito2)
-print(producto1)
+# producto1 = Producto(id_producto=1,nombre="Laptop",precio=150.00,stock=10)
+# print(producto1)
+# producto2 = Producto(id_producto=2,nombre="Monitor LG",precio=200.00,stock=20)
+# print(producto2)
+# print("--------------")
+# # disminuir stock
+# exito= producto1.reducir_stock(3)
+# print("reduccion exitosa?" , exito)
+# print(producto1)
+# print("-------------------")
+# exito2= producto1.aumento_stock(3)
+# print("reduccion exitosa?" , exito2)
+# print(producto1)
 
 # una clase pueede ser u tipo de dato (producto:Producto)
 class CarritoCompras:
     def __init__(self , usuario_id :int):
         self.usuario_id=usuario_id
-        self.productos=List[dict]=[]
+        self.productos:List[dict]=[]
         self.fecha_creacion=datetime.now()
     def agregar_producto(self ,producto:Producto, cantidad:int)->bool:
         if producto.stock>=cantidad:
@@ -58,8 +58,32 @@ class CarritoCompras:
         for item in self.productos:
             total+=item['precio_unitario']*item['cantidad']
         return total
+    def obtener_resumen(self)->str:
+        resumen=f"carrito del usuario {self.usuario_id}:\n"
+        for item in self.productos:
+            producto=item["producto"]
+            cantidad=item["cantidad"]
+            subtotal=item["precio_unitario"]*cantidad
+            resumen+=f"{producto.nombre}x{cantidad}={subtotal}\n"
+        resumen+=f"total : ${self.calcular_total():.2f}"
+        return resumen
+    def confirmar_compra(self):
+        for item in self.productos:
+            producto =item['producto']
+            cantidad =item['cantidad']
+            producto.reducir_stock(cantidad)
     
+# crear productos
+producto1=Producto(1,"laptop",1500.0,10)
+producto2=Producto(2,"mouse",25.0,50)
+producto3=Producto(3,"teclado",45.0,30)
 
+carrito=CarritoCompras(usuario_id=1001)
+carrito.agregar_producto(producto1,1)
+carrito.agregar_producto(producto2,2)
+carrito.agregar_producto(producto3,1)
 
+total=carrito.calcular_total()
+print(f"total carrito: ${total:.2f}")
 
-
+# 2f->es para que saque el dat con 2 decimales
